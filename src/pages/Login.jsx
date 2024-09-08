a846d1a5f6e139bbc6c320dae72eddad19b031c6
duvan <esparzaduvan3@gmail.com>
2024-09-08 11:57:58 -0500

    Initial commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import '../styles/Login.css'
+
+
+export const Login = () => {
+    return (
+        <div className="container_principal">
+            <img className="avion" src="" alt="logo avion" />
+            <div className="container_world">
+                <img src="" alt="img mundo" />
+            </div>
+        </div>
+    )
+}
